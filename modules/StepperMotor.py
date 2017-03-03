4a49052f2c7e9474a74ad2c6fa29759042dcf96b
Daniel Glownia <dglownia222@gmail.com>
2017-03-02 22:48:18 -0600

    sdfsdf

--- modules/StepperMotor.py
+++ modules/StepperMotor.py
@@ -22,43 +22,7 @@ Seq = [[1,0,0,1],
 
 StepCount = len(Seq)
 StepDir = 1
-
-# Initialise variables
 StepCounter = 0
-
-counter = 0
-run = True
-# Start main loop
-while run:
-
-  print StepCounter,
-  print Seq[StepCounter]
-
-  for pin in range(0,4):
-    xpin=StepPins[pin]# Get GPIO
-    if Seq[StepCounter][pin]!=0:
-      print " Enable GPIO %i" %(xpin)
-      GPIO.output(xpin, True)
-    else:
-      GPIO.output(xpin, False)
-
-  StepCounter += StepDir
-  counter = counter + 1
-  # If we reach the end of the sequence
-  # start again
-  if (StepCounter>=StepCount):
-    StepCounter = 0
-  if (StepCounter<0):
-    StepCounter = StepCount+StepDir
-  if (counter == 1000000):
-      run = False
-  # Wait before moving on
-  time.sleep(10/float(1000))
-
-
-
-
-
 
 with open('config/HardwareConfig.json') as data_file:
     hardwareData = json.load(data_file)
@@ -67,7 +31,35 @@ def getId():
 
 def update(state):
     try:
-        print('ok')
+        counter = 0
+        run = True
+        # Start main loop
+        while run:
+            print StepCounter,
+            print Seq[StepCounter]
+
+            for pin in range(0,4):
+            xpin=StepPins[pin]# Get GPIO
+            if Seq[StepCounter][pin]!=0:
+              print " Enable GPIO %i" %(xpin)
+              GPIO.output(xpin, True)
+            else:
+              GPIO.output(xpin, False)
+
+            StepCounter += StepDir
+            counter = counter + 1
+            # If we reach the end of the sequence
+            # start again
+            if (StepCounter>=StepCount):
+            StepCounter = 0
+            if (StepCounter<0):
+            StepCounter = StepCount+StepDir
+            if (counter == 1000000):
+              run = False
+            # Wait before moving on
+            time.sleep(10/float(1000))
+        return True
+        
     except:
         return False
 
